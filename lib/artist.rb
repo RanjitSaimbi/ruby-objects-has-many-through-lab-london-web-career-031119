e0ee8eb5a07c386e35bf3bcc014e74b398f28ffa
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-15 09:59:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,32 @@
 class Artist 
   
-  # @@all = []
+  @@all = []
 
-  # def self.all
-  #   @@all 
-  # end 
+  def self.all
+    @@all 
+  end 
   
-  # attr_accessor :name 
+  attr_accessor :name 
    
-  # def initialize(name)
-  #   @name = name
-  #   @@all << self 
-  # end 
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
   
-  # def new_song(song_name, genre) 
-  #   Song.new(song_name, self, genre)
-  # end
+  def new_song(song_name, genre) 
+    Song.new(song_name, self, genre)
+  end
   
-  # def songs 
-  #   Song.all.select.do |song|
-  #     song.artist == self 
-  #   end 
-  # end 
+  def songs 
+    Song.all.select.do |song|
+      song.artist == self 
+    end 
+  end 
   
-  # def genres 
-  #   songs.map do |song|
-  #     songs.genre
-  #   end 
-  # end 
+  def genres 
+    songs.map do |song|
+      songs.genre
+    end 
+  end 
    
 end
