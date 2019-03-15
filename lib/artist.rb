72b45feef5878b2df3170bc527d2be91cee6527b
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-15 10:00:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,20 +13,20 @@ class Artist
     @@all << self 
   end 
   
-  def new_song(song_name, genre) 
-    Song.new(song_name, self, genre)
-  end
+  # def new_song(song_name, genre) 
+  #   Song.new(song_name, self, genre)
+  # end
   
-  def songs 
-    Song.all.select.do |song|
-      song.artist == self 
-    end 
-  end 
+  # def songs 
+  #   Song.all.select.do |song|
+  #     song.artist == self 
+  #   end 
+  # end 
   
-  def genres 
-    songs.map do |song|
-      songs.genre
-    end 
-  end 
+  # def genres 
+  #   songs.map do |song|
+  #     songs.genre
+  #   end 
+  # end 
    
 end
