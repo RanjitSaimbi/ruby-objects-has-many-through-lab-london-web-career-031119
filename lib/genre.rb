db5c43f36efcda5d4c9deecd632ffda7accc07b8
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-15 10:02:03 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,26 @@
 class Genre 
-  @@all = []
-  attr_accessor :name
+  # @@all = []
+  # attr_accessor :name
   
-  def self.all
-    @@all 
-  end 
+  # def self.all
+  #   @@all 
+  # end 
   
-  def initialize(name)
-    @name = name
-    @@all << self 
-  end 
+  # def initialize(name)
+  #   @name = name
+  #   @@all << self 
+  # end 
   
-  def songs 
-    Song.all.select.do |song|
-      song.genre == self 
-    end 
-  end
+  # def songs 
+  #   Song.all.select.do |song|
+  #     song.genre == self 
+  #   end 
+  # end
   
-  def artists
-    songs.map do |song|
-      songs.artist 
-    end
-  end
+  # def artists
+  #   songs.map do |song|
+  #     songs.artist 
+  #   end
+  # end
   
 end 
